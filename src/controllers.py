b3a28e5b23085be9a2ae86995a6424eed3e5186d
Feverfew <alexlns99@yahoo.co.uk>
2016-02-21 23:03:59 +0000

    done for the weekend

--- src/controllers.py
+++ src/controllers.py
@@ -16,9 +16,32 @@ class ChessBoardController(QtGui.QWidget, views.ChessBoard):
         super(ChessBoardController, self).__init__()
         self.setupUi(self)
         self.board = Board()
+        self.initialise_board()
+        self.output_board()
 
-    def intialise_board(self):
-        pass
+    def initialise_board(self):  
+        self.chess_board.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        self.chess_board.verticalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        self.chess_board.horizontalHeader().hide()
+        self.chess_board.verticalHeader().hide()
+        for y in range(8):
+            for x in range(8):
+                item = QtGui.QTableWidgetItem()
+                if (x+y) % 2 == 0:
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(31, 177, 209)))
+                else:
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(11, 129, 156)))
+                self.chess_board.setItem(y, x, item)
 
     def output_board(self):
-        pass
+        """Output the board onto the GUI"""
+        for y in range(8):
+            for x in range(8):
+                item = QtGui.QTableWidgetItem()
+                if (x+y) % 2 == 0:
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(31, 177, 209))) # light
+                else:
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(11, 129, 156))) # dark
+                if self.board.board[y][x]:
+                    item.setText(str(self.board.board[y][x]))
+                self.chess_board.setItem(y, x, item)
