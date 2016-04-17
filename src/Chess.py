7c8709b3a7f775c785e9c7bd89cf950cdb64cfe3
Feverfew <alexlns99@yahoo.co.uk>
2016-04-17 20:47:20 +0100

    commited

--- src/Chess.py
+++ src/Chess.py
@@ -5,7 +5,6 @@ from PySide import QtGui, QtCore
 if __name__ == "__main__":
 
     app = QtGui.QApplication(sys.argv)  
-
     win  = controllers.MainWindowController() 
 
     app.connect(app, QtCore.SIGNAL("lastWindowClosed()"),
